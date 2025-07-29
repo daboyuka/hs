06a50df5052bd44ddcac38cc32780482212618e6
Drew Boyuka <drew@pendo.io>
2025-07-29 15:47:11 -0400

    Move to more normalized build filenames in release.sh

--- release.sh
+++ release.sh
@@ -3,15 +3,18 @@
 # Builds binaries for several OSs/archs, with release info baked in via ldflags.
 
 SHA=${1:-$(git rev-parse HEAD)}
-TAG=${2:-$(git describe --exact-match HEAD 2>/dev/null || echo "")}
+TAG=${2:-$(git describe --exact-match --tags HEAD 2>/dev/null || echo "")}
 SHADATE=$(TZ=UTC0 git show -s --date='format-local:%Y-%m-%dT%H:%M:%SZ' --format="%cd" $SHA)
-
 
 RPKG=github.com/daboyuka/hs/release
 LDFLAGS=(
   -ldflags "-X $RPKG.Tag=$TAG -X $RPKG.Commit=$SHA -X $RPKG.DateRaw=$SHADATE"
 )
 
-GOOS=linux GOARCH=amd64 go build "${LDFLAGS[@]}" -o dist/hs.linux
-GOOS=darwin GOARCH=amd64 go build "${LDFLAGS[@]}" -o dist/hs.mac
-GOOS=darwin GOARCH=arm64 go build "${LDFLAGS[@]}" -o dist/hs.m1mac
+function mkrelease() {
+  GOOS=$1 GOARCH=$2 go build "${LDFLAGS[@]}" -o dist/hs_${1}_${2}
+}
+
+mkrelease linux amd64
+mkrelease darwin amd64
+mkrelease darwin arm64
